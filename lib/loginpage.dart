6f0f1747365db77fba9bd53a83d12de870461b96
ujas3279 <ujas6520@gmail.com>
2020-02-26 23:22:30 +0530

    1

--- lib/loginpage.dart
+++ lib/loginpage.dart
@@ -1,40 +1,41 @@
+import 'package:canteen_management/home_screen.dart';
 import 'package:flutter/material.dart';
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
-
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
-
 class _MyHomePageState extends State<MyHomePage> {
   TextStyle style = TextStyle(fontFamily: 'Montserrat', fontSize: 20.0);
-
+  String user_name=" ";
+  String password=" ";
+  final _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
    
 
-    final emailField = TextField(
-      obscureText: true,
+    final emailField = TextFormField(
+      
       style: style,
       decoration: InputDecoration(
           contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
-          hintText: "Email",
+          hintText: "User Name",
           border:
               OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
+      onChanged: (val){
+          user_name=val;
+      },
+      validator: (value) {
+          if (value.isEmpty) {
+               return 'Please Enter Username';
+               }
+        return null;
+  },
+     
     );
 
-    final passwordField = TextField(
+    final passwordField = TextFormField(
       obscureText: true,
       style: style,
       decoration: InputDecoration(
@@ -42,6 +43,15 @@ class _MyHomePageState extends State<MyHomePage> {
           hintText: "Password",
           border:
               OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
+      onChanged: (val){
+        password=val;
+      },
+      validator: (value) {
+       if (value.isEmpty) {
+        return 'Please enter Password';
+      }
+      return null;
+     },
     );
 
     final loginButon = Material(
@@ -51,7 +61,7 @@ class _MyHomePageState extends State<MyHomePage> {
       child: MaterialButton(
         minWidth: MediaQuery.of(context).size.width,
         padding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
-        onPressed: () {},
+        onPressed: Check_Login,
         child: Text("Login",
             textAlign: TextAlign.center,
             style: style.copyWith(
@@ -64,12 +74,16 @@ class _MyHomePageState extends State<MyHomePage> {
       child: Center(
         child: Container(
           color: Colors.white,
-          child: Padding(
-            padding: const EdgeInsets.all(36.0),
-            child: Column(
+          child: Form(
+            key: _formKey,
+            child: Padding(
+              padding: EdgeInsets.all(36.0),
+              child: Column(
               crossAxisAlignment: CrossAxisAlignment.center,
               mainAxisAlignment: MainAxisAlignment.center,
+              
               children: <Widget>[
+                
                 SizedBox(
                   height: 155.0,
                   child: Image.asset(
@@ -88,13 +102,29 @@ class _MyHomePageState extends State<MyHomePage> {
                 SizedBox(
                   height: 15.0,
                 ),
+               
               ],
             ),
           ),
         ),
+              ),
+            ),
+            
       ),
-        )
+        
     );
   }
-   
+  void Check_Login(){
+    if (_formKey.currentState.validate()){
+          if(user_name=="admin"){
+                 if(password=='admin'){
+                       Navigator.push(context,  MaterialPageRoute(builder: (context)=>new HomeScreen()));        
+                 }
+                 else
+                   print('incorrect password');
+          }
+          else
+            print('incorrect username or password');
+  } 
+  }
 }
