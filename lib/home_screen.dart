9bd2b05dafbbe2c7ad5b815b9fdb7ae8aa0a61b4
ujas3279 <ujas6520@gmail.com>
2020-03-04 13:11:28 +0530

    1

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,3 +1,6 @@
+
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 class HomeScreen extends StatefulWidget {
   @override
@@ -5,11 +8,33 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  Future<bool> _onBackPressed()  {
+   return showDialog(
+    context: context,
+     builder :(context)=> AlertDialog(
+        title: Text('Do You want To Exit App?'),
+        actions: <Widget>[
+          FlatButton(
+            onPressed: ()=>exit(0), 
+            child: Text('Yes')),
+          FlatButton(
+            onPressed: ()=>Navigator.pop(context,false), 
+            child: Text('No'))
+        ],  
+     ), 
+   );
+}
   @override
+  
   Widget build(BuildContext context) {
-    return Scaffold(
+    return WillPopScope(
+      onWillPop: _onBackPressed,
+      child:  new Scaffold(
+     
       appBar: AppBar(title: Text('My Canteen'),),
       
+    ),
     );
   }
+  
 }
